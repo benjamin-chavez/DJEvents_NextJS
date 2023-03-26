215fad8c54f560118f4b65c6bf605a071d8a6af3
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-26 11:32:09 -0500

    Added the landing page showcase component

---
+++ frontend/components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Showcase</h1>
+      <h2>Find the hottest Live events</h2>
+    </div>
+  );
+}
