52264b0f25e98e3ea6272c060e1186cb40877cb0
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-26 12:01:11 -0500

    worked with getStaticProps and getServerSideProps

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,35 @@
 import Head from 'next/head';
 import Layout from '@/components/Layout';
+// import { API_URL } from '@/config/index';
+import { API_URL } from '@/config';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  // console.log(events);
+
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+// getServerSideProps runs everytime we load the page
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+//   return {
+//     props: { events },
+//   };
+// }
+
+// getStaticSideProps runs at build time only
+export async function getStaticSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
