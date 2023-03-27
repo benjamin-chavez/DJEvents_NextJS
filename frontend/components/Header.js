f13d1c0beccf794d3bdb208544d646e6044a6c37
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-27 15:37:35 -0500

    Added Register & Login Form Pages

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Search from './Search';
 import styles from '@/styles/Header.module.css';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
 export default function Header() {
   return (
@@ -22,9 +23,19 @@ export default function Header() {
               {/* <a>Events</a> */}
             </Link>
           </li>
+
           <li>
             <Link href="/events/add" legacyBehavior>
               <a>Add Event</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/account/login" legacyBehavior>
+              <a className="btn=secondary btn-icon">
+                <FaSignInAlt />
+                Login
+              </a>
             </Link>
           </li>
         </ul>
