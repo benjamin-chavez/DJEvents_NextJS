920b00ec8e780f7e2ea856073e8c07ca66e1bd4e
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-27 16:01:06 -0500

    Added ContextAPI and AuthContext

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,13 @@
-import Link from 'next/link';
-import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.css';
+import Link from 'next/link';
+import { useContext } from 'react';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import Search from './Search';
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -24,20 +28,43 @@ export default function Header() {
             </Link>
           </li>
 
-          <li>
-            <Link href="/events/add" legacyBehavior>
-              <a>Add Event</a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              {/* IF USER LOGGED IN */}
+              <li>
+                <Link href="/events/add" legacyBehavior>
+                  <a>Add Event</a>
+                </Link>
+              </li>
 
-          <li>
-            <Link href="/account/login" legacyBehavior>
-              <a className="btn=secondary btn-icon">
-                <FaSignInAlt />
-                Login
-              </a>
-            </Link>
-          </li>
+              <li>
+                <Link href="/account/dashboard" legacyBehavior>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              {/* IF NO USER */}
+              <li>
+                <Link href="/account/login" legacyBehavior>
+                  <a className="btn=secondary btn-icon">
+                    <FaSignInAlt />
+                    Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
